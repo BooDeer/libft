ee2a71f313a96cfcc54ca906e6266fa36bb0460d
Houssam-eddine Boudhir <hboudhir@e2r6p8.1337.ma>
2019-11-29 18:58:16 +0100

    libft

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 23:27:34 by hboudhir          #+#    #+#             */
-/*   Updated: 2019/11/27 23:29:30 by hboudhir         ###   ########.fr       */
+/*   Updated: 2019/11/29 18:58:02 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list *temp;
     
     temp = (t_list*)malloc(sizeof(t_list));
-    temp->data = 10;
-    temp->link = NULL;
-    if (*root == NULL)
-        *root = temp;
+    temp->content = 10;
+    temp->next = NULL;
+    if (*lst == NULL)
+        *lst = temp;
     else
     {
         t_list *p;
-        p = *root;
-        while (p->link != NULL)
-            p = p->link;
-        p->link = temp;            
+        p = *lst;
+        while (p->next != NULL)
+            p = p->next;
+        p->next = temp;            
     }  
 	
 	
